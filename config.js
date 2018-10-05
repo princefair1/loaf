31f30313d53f35fbe344a410a74c17dc5eb5d563
princefair1 <42935207+princefair1@users.noreply.github.com>
2018-10-05 15:56:03 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 {
-  "ownerID": "the discord user id of the bot owner",
-  "botToken": "the authentication token of the bot"
+  "ownerID": "368324156367962112",
+  "botToken": "NDk3MzU0MzM5OTA0NjUxMjY2.Dpj-4w.aTLrJbvvXPNH0Kp1Are07J5Eduo"
 }
